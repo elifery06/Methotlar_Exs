7d77ae2ff65814a149fe76b62ffb2781c13b128b
elifery06 <75325359+elifery06@users.noreply.github.com>
2021-03-29 23:40:19 +0300

    Add files via upload

---
+++ methotlar/methotlar/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace methotlar
+{
+    class Program
+    { 
+        
+        
+
+    static void Main(string[] args)
+        {
+            emlak eml = new emlak();
+            eml.SEMTİ = "Keçiören";
+            eml.Rengi = "mavi";
+            eml.ODASAYİSİ = 4;
+            eml.KATNO = 5;
+            eml.ALAN = 120;
+            Console.WriteLine("evin bulunduğu semt: " + eml.SEMTİ);
+            Console.WriteLine("evin oda renleri: "+eml.Rengi );
+            Console.WriteLine("evin toplamoda sayısı: "+eml.ODASAYİSİ);
+            Console.WriteLine("evin bulunduğu kat: "+eml.ODASAYİSİ);
+            Console.WriteLine("evin toplam alanı: "+eml.ALAN+" metrekare");
+            Console.Read();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ methotlar/methotlar/emlak.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methotlar
+{
+    class emlak
+    {
+        private string semt;
+        private string renk;
+        private int odasayi;
+        private int katno;
+        private double alan;
+
+        public string SEMTİ
+        {get { return semt; }
+            set { semt = value.ToUpper(); }
+
+        }
+        public string Rengi
+        {
+            
+            get { return renk; }
+            set { renk = value.ToLower(); }
+        }
+        public int ODASAYİSİ
+        {
+            get { return odasayi; }
+            set { odasayi = Math.Abs(value); }
+        }
+        public int KATNO
+        {
+            get { return katno; }
+            set { katno = Math.Abs(value); }
+        }
+        public Double ALAN
+        {
+            get { return alan; }
+            set { alan = Math.Abs(value); }
+        }
+    }
+}
